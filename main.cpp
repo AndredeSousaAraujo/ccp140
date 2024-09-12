d15b31c5df554e72b66f45cb37bda9a34de4c5d8
André Araujo <79666573+AndredeSousaAraujo@users.noreply.github.com>
2024-09-12 01:22:24 +0000

    Classe Data foi incluída

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main(){
-    Pessoa p;
-    p.setNome("Nome");
-    p.setSobrenome("Sobrenome");
-    p.setCPF(12345678900);
+    Pessoa p("Andre", "Araujo", 12345678900, 11, 9, 2024);
+    //p.setNome("Nome");
+    //p.setSobrenome("Sobrenome");
+    //p.setCPF(12345678900);
     cout << p.getNome() << endl;
     cout << p.getSobrenome() << endl;
     cout << p.getCPF() << endl;
+    cout << p.getNascimento() << endl;
 }
