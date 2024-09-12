f15f45f7d81aaad1c7ad26fdb1d9083a7d26d126
André Araujo <79666573+AndredeSousaAraujo@users.noreply.github.com>
2024-09-12 00:49:34 +0000

    Setter e Getter da classe Pessoa

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "Pessoa.cpp"
+using namespace std;
 
 int main(){
-    std::cout << "Hello, world!\n";
+    Pessoa p;
+    p.setNome("Nome");
+    p.setSobrenome("Sobrenome");
+    p.setCPF(12345678900);
+    cout << p.getNome() << endl;
+    cout << p.getSobrenome() << endl;
+    cout << p.getCPF() << endl;
 }
